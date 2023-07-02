92660835f7461b1367cd03d776afb4304e5647c4
Muhammad Mustafa <muhammadmustafa@Muhammads-MacBook-Air.local>
2023-07-02 13:36:15 +0500

    FirstAssignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Experience from './components/experience';
+import Education from './components/education';
+import Skill from './components/skill';
+
 function App() {
+
+  var obj = {
+    data: ["1 year"]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Education></Education>
+    <Experience {...obj} ></Experience>
+    <Skill></Skill>
+    </>
   );
 }
 
